241350296ee7847ce031eb6edabeff78b63f567c
mendess <pedro.mendes.26@gmail.com>
2022-07-04 02:49:33 +0100

    cache card names and show them

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,160 @@
-use notify_rust::Notification;
-use scryfall::card::Card;
+use anyhow::Context;
+use notify_rust::{Notification, Urgency};
+use scryfall::{
+    card::{Card, Game},
+    error::ScryfallError,
+    set::{SetCode, SetType},
+    Error, Set,
+};
 use std::{
+    fmt::Display,
+    fs::{self, File},
+    io::{self, BufRead, BufReader, BufWriter, Write},
     os::unix::prelude::ExitStatusExt,
+    path::Path,
     process::{Command, Stdio},
 };
 use tempfile::NamedTempFile;
 
+fn notify<T, B>(title: T, body: B)
+where
+    T: Display,
+    B: Display,
+{
+    Notification::new()
+        .summary(&format!("{title}"))
+        .body(&format!("{body}"))
+        .urgency(Urgency::Low)
+        .show()
+        .unwrap();
+}
+
+fn error(e: anyhow::Error) {
+    Notification::new()
+        .summary("Error foretelling")
+        .body(&format!("{e:?}"))
+        .urgency(Urgency::Critical)
+        .show()
+        .unwrap();
+}
+
+fn missing_sets(sets_cache: &Path, cards_cache: &Path) -> anyhow::Result<()> {
+    let stored_sets = match File::open(sets_cache) {
+        Ok(f) => BufReader::new(f).lines().collect::<Result<Vec<_>, _>>()?,
+        Err(e) if e.kind() == io::ErrorKind::NotFound => vec![],
+        Err(e) => return Err(e.into()),
+    };
+    let mut all_sets = Vec::new();
+    let mut updated_cards = false;
+    let today = chrono::Utc::today().naive_utc();
+    for Set { code, name, .. } in scryfall::set::Set::all()?
+        .filter_map(Result::ok)
+        .filter(|s| matches!(s.released_at, Some(d) if d <= today))
+    {
+        all_sets.push(code);
+        if stored_sets
+            .binary_search_by(|s| s.as_str().cmp(code.get()))
+            .is_err()
+        {
+            match update_card_list(cards_cache, code, &name)
+                .with_context(|| format!("updating card list for set {name} ({code})"))
+            {
+                Ok(true) => updated_cards = true,
+                Ok(false) => {
+                    all_sets.pop();
+                }
+                Err(e) => {
+                    all_sets.pop();
+                    error(e);
+                }
+            }
+        }
+    }
+    if updated_cards {
+        all_sets.sort();
+        store_sets(sets_cache, &all_sets)?;
+    }
+    return Ok(());
+
+    fn store_sets(cache: &Path, sets: &[SetCode]) -> anyhow::Result<()> {
+        sets.iter()
+            .try_fold(BufWriter::new(File::create(&cache)?), |mut file, code| {
+                file.write_all(code.get().as_bytes())?;
+                file.write_all(b"\n")?;
+                io::Result::Ok(file)
+            })?;
+        Ok(())
+    }
+}
+
+/// Updates the card list
+///
+/// if no cards were found for a set, return Ok(false)
+/// this happens when new sets are added before their cards are added.
+fn update_card_list(path: &Path, set_code: SetCode, set_name: &str) -> anyhow::Result<bool> {
+    let mut file = BufWriter::new(
+        File::options()
+            .create(true)
+            .append(true)
+            .open(&path)
+            .with_context(|| format!("opening card list at {path:?}"))?,
+    );
+
+    const JUST_DONT: &str = "Our Market Research Shows That Players Like Really Long Card Names So We Made this Card to Have the Absolute Longest Card Name Ever Elemental";
+    use scryfall::search::prelude::*;
+    let today = chrono::Utc::today().naive_utc();
+    let count = match Card::search(
+        set(set_code)
+            .and(not(set_type(SetType::Memorabilia)))
+            .and(not(set_type(SetType::Token)))
+            .and(game(Game::Paper))
+            .and(date(lte(today))),
+    ) {
+        Ok(cards) => cards
+            .filter_map(Result::ok)
+            .filter(|c| !c.type_line.contains("Basic") && !c.type_line.contains("Token"))
+            .map(|c| c.name)
+            .filter(|n| n != JUST_DONT)
+            .try_fold(0, |count, name| {
+                file.write_fmt(format_args!("{name}\n"))?;
+                io::Result::Ok(count + 1)
+            })?,
+        Err(Error::ScryfallError(ScryfallError { status: 404, .. })) => return Ok(false),
+        Err(e) => return Err(e.into()),
+    };
+
+    notify(
+        format!("Set {set_name} ({set_code}) added!"),
+        format!("{count} new cards added!"),
+    );
+    Ok(true)
+}
+
+fn card_list() -> anyhow::Result<File> {
+    let path = dirs::cache_dir()
+        .ok_or_else(|| anyhow::anyhow!("can't find cache dir"))?
+        .join("foretell");
+
+    fs::create_dir_all(&path).context("creating cache dir")?;
+
+    println!("getting missing sets");
+    let sets_cache = path.join("sets");
+    let card_list_file = path.join("cards");
+    missing_sets(&sets_cache, &card_list_file).context("getting missing sets")?;
+
+    Ok(File::open(card_list_file)?)
+}
+
 fn query() -> anyhow::Result<String> {
     let output = Command::new("dmenu")
-        .args(["-p", "scry"])
-        .stdin(Stdio::null())
+        .args(["-p", "scry", "-l", "30", "-i"])
+        .stdin(match card_list() {
+            Ok(f) => Stdio::from(f),
+            Err(e) => {
+                error(e);
+                Stdio::null()
+            }
+        })
         .output()?;
 
     if output.status.success() {
@@ -28,10 +173,10 @@ fn query() -> anyhow::Result<String> {
 
 fn run() -> anyhow::Result<()> {
     let query = dbg!(query()?);
+    if query.is_empty() {
+        return Ok(());
+    }
     let cards = Card::search(&query)?
-        .inspect(|c| {
-            dbg!(c);
-        })
         .map(|c| {
             c.map(|mut c| {
                 if let Some(faces) = c.card_faces.take() {
@@ -48,7 +193,7 @@ fn run() -> anyhow::Result<()> {
                 }
             })
         })
-        .try_fold(Vec::new(), |mut acc, v| -> Result<_, scryfall::Error> {
+        .try_fold(Vec::new(), |mut acc, v| -> Result<_, Error> {
             acc.extend(v?);
             Ok(acc)
         })?;
@@ -73,10 +218,6 @@ fn run() -> anyhow::Result<()> {
 
 fn main() {
     if let Err(e) = run() {
-        Notification::new()
-            .summary("Error foretelling")
-            .body(&format!("{e:?}"))
-            .show()
-            .unwrap();
+        error(e)
     }
 }
